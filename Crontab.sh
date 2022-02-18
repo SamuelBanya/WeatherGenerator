c36815877891aeecc898a8402cc9440dae1e59fe
SamuelBanya <90813277+SamuelBanya@users.noreply.github.com>
2022-02-18 18:17:40 -0500

    Adding older crontab job to model for future Bash or Python 3 script

---
+++ Crontab.sh
@@ -0,0 +1,8 @@
+*/5 * * * * wget -O /var/www/musimatic/images/newyorkweather.png wttr.in/newyork.png
+*/5 * * * * wget -O /var/www/musimatic/images/nashvilleweather.png wttr.in/nashville.png
+*/5 * * * * wget -O /var/www/musimatic/images/scrantonweather.png wttr.in/scranton.png
+*/5 * * * * wget -O /var/www/musimatic/images/poincianaweather.png wttr.in/poinciana.png
+*/5 * * * * wget -O /var/www/musimatic/images/newyorkweather.gif https://radar.weather.gov/ridge/lite/KOKX_loop.gif
+*/5 * * * * wget -O /var/www/musimatic/images/nashvilleweather.gif https://radar.weather.gov/ridge/lite/KOHX_loop.gif
+*/5 * * * * wget -O /var/www/musimatic/images/scrantonweather.gif https://radar.weather.gov/ridge/lite/KBGM_loop.gif
+*/5 * * * * wget -O /var/www/musimatic/images/poincianaweather.gif https://radar.weather.gov/ridge/lite/KTBW_loop.gif
